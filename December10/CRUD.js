4735bc3bf2f1865a10024798744ad060a68372f4
Bhavleen Singh <singhbhavleen3@gmail.com>
2020-12-10 14:43:13 +0530

    Crud using only one function

---
+++ December10/CRUD.js
@@ -0,0 +1,18 @@
+let names = ["vinod", "binod", "vinodh", "vinode"];
+
+// Add binodh at the end (CREATE)
+// Tell me output of the splice method (READ)
+// Update vinode to vinodji (UPDATE)
+// Delete vinod from an array (DELETE)
+// Using only one function
+
+// CREATE
+names.splice(5, 5, "binodh");
+// READ
+console.log("Create and Read ", names);
+// UPDATE
+names.splice(3, 1, "vinodji");
+console.log("Update", names);
+// DELETE
+names.splice(0, 1);
+console.log("Delete", names);
