e19fcab5a6f45b187b46f04b225fd77c6fbd8cac
Bhavleen Singh <singhbhavleen3@gmail.com>
2020-12-10 15:07:29 +0530

    Learning Array functin

--- December10/CRUD.js
+++ December10/CRUD.js
@@ -2,7 +2,7 @@ let names = ["vinod", "binod", "vinodh", "vinode"];
 
 // Add binodh at the end (CREATE)
 // Tell me output of the splice method (READ)
-// Update vinode to vinodji (UPDATE)
+// Update vinode to VINODE (UPDATE)
 // Delete vinod from an array (DELETE)
 // Using only one function
 
@@ -11,7 +11,7 @@ names.splice(5, 5, "binodh");
 // READ
 console.log("Create and Read ", names);
 // UPDATE
-names.splice(3, 1, "vinodji");
+names.splice(3, 1, "VINODE");
 console.log("Update", names);
 // DELETE
 names.splice(0, 1);
